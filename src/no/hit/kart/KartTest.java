716f3c297cec4660906b76eda5d3ba1617ad6748
Patrick Pedersen <patrick@ossia.net>
2017-09-06 13:02:56 +0200

    Test for Hendelse

--- src/no/hit/kart/KartTest.java
+++ src/no/hit/kart/KartTest.java
@@ -15,10 +15,16 @@ public class KartTest {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
+    
+    //Punkt test
     Punkt A = new Punkt(30,700);
     Punkt B = new Punkt(300,86);
-    System.out.println(A.avstand(B));
-    System.out.println(A);
+    //System.out.println(A.avstand(B));
+    //System.out.println(A);
+    
+    //Hendelse test
+    Hendelse C = new Hendelse(new Dato(23,05,2017),B,"Kul tur");
+    //System.out.println(C.toString());
   }
   
 }
