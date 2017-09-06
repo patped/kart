4ca9be85fb3e762ab4949ecddb50809ac30ad0d9
Patrick Pedersen <patrick@ossia.net>
2017-09-06 12:43:13 +0200

    Made test file

---
+++ src/no/hit/kart/KartTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.hit.kart;
+
+/**
+ *
+ * @author patrick
+ */
+public class KartTest {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    Punkt A = new Punkt(30,700);
+    Punkt B = new Punkt(300,86);
+    System.out.println(A.avstand(B));
+    System.out.println(A);
+  }
+  
+}
